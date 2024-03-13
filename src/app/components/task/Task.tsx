8cc2fda75ec782ef0ae2a8310655e10b2fa8bca3
ErikSkole <e-erijoh6@iskule.no>
2024-03-13 16:13:28 +0100

    First commit

---
+++ src/app/components/task/Task.tsx
@@ -0,0 +1,17 @@
+export default function Task() {
+    return (
+        <>
+            <div className="taskBox">
+                <div className="taskBoxColor"></div>
+                <div className="taskBoxContent flex flex-col p-4">
+                    <div className="taskBoxHeader">
+                        <h2>Test</h2>
+                    </div>
+                    <div className="taskBoxTextContent">
+                        <p>Dette er en test for å se om dette funker</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
